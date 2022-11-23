f74ada10d6d25271a34743ee85285fd53ee38160
O`rinboyev Barkamol <97020905+barkamoljon@users.noreply.github.com>
2022-11-23 11:45:47 +0500

    Create 4-amaliyot.py

---
+++ 4-amaliyot(5-dars)/4-amaliyot.py
@@ -0,0 +1,16 @@
+#1
+salom_dunyo = "Hello World!"
+print(salom_dunyo)
+#2
+xabar = 'Assalomu alaykum'
+print(xabar)
+xabar = 'Va alaykum assalom va rohmatulloh'
+print(xabar)
+#3
+#class = "ishlamaydi"
+#print(class)
+#4
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
