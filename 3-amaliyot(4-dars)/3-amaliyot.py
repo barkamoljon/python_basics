d0d0b3c250194227410936907beb39a9697833a6
O`rinboyev Barkamol <97020905+barkamoljon@users.noreply.github.com>
2022-11-23 11:47:41 +0500

    Create 3-amaliyot.py

---
+++ 3-amaliyot(4-dars)/3-amaliyot.py
@@ -0,0 +1,12 @@
+#1
+print("\"Nexia\", \"Tico\", 'Damas' ko`rganlar qilar havas")
+#2 5 ning 4-darajasi 
+print("5 ning 4-darajasi", 5**4)
+#3 22 ni 4ga bo`lgandagi qoldig`i
+print("22 ni 4ga bo`lgandagi qoldig`i", 22%4)
+#4 Tomonlari 125ga teng kvadratning yuzi va perimetri
+print("Tomonlari 125ga teng kvadratning yuzi",125**2,'perimetri',125*4)
+#5 Diametri 12 ga teng bo'lgan doiraning yuzi ( π=3.14 deb oling)
+print("Diametri 12 ga teng bolgan doiraning yuzi",12*3.14)
+#6 Katetlari 6 va 7 bo`lgan to`g`ri burchakli uchburchakning gipotenuzasini topish
+print("Katetlari 6 va 7 bo`lgan to`g`ri burchakli uchburchakning gipotenuzasi", ((6**2+7**2)**0.5))
