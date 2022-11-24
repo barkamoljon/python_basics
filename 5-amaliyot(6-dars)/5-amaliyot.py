23515bdec583e541a60f6d325caf120c54afd4d4
O`rinboyev Barkamol <97020905+barkamoljon@users.noreply.github.com>
2022-11-24 11:56:22 +0500

    Create 5-amaliyot.py

---
+++ 5-amaliyot(6-dars)/5-amaliyot.py
@@ -0,0 +1,20 @@
+kocha="Bog'bon"
+mahalla="Sog'bon"
+tuman="Bodomzor"
+viloyat="Samarqand"
+#2
+print(kocha + " ko'chasi",mahalla+ " mahallasi",tuman+ " tumani",viloyat+" viloyati")
+#3
+kocha = input('Ko`cha nomini kiriting:')
+mahalla = input("Mahalla nomini kiriting: ")
+tuman = input("Tuman nomini kiriting: ")
+viloyat = input("Viloyat nomini kiriting: ")
+print(kocha + " ko'chasi",mahalla+ " mahallasi",tuman+ " tumani",viloyat+" viloyati")
+#4
+print(kocha + "ko'chasi\n",mahalla+ "mahallasi\n",tuman+ "tumani\n",viloyat+"viloyati\n")
+#5
+manzil = (f"{kocha} ko'chasi, {mahalla} mahallasi,{tuman} tumani, {viloyat} viloyati")
+print(manzil.title())
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.capitalize())
