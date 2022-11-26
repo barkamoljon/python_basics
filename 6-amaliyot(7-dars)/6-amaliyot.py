4bdd40d5fe38da0417e561b4e6c45f20eb9f32d0
O`rinboyev Barkamol <97020905+barkamoljon@users.noreply.github.com>
2022-11-26 06:23:32 +0500

    Create 6-amaliyot.py

---
+++ 6-amaliyot(7-dars)/6-amaliyot.py
@@ -0,0 +1,15 @@
+#Bismilillah
+#1
+x = int(input('Istalgan butun son kiriting:\n>>>'))
+print(f'{x} ning kvadrati {x**2} ga teng')
+print(f'{x} ning kubi {x**3} ga teng')
+#2
+yosh = int(input('Yoshingizni kiriting:'))
+print(f'Siz {2022-yosh}-yil da tug`ilgansiz')
+#3
+x = float(input("Birinchi sonni kiriting: "))
+y = float(input("Ikkinchi sonni kiriting: "))
+print(f'{x}+{y}')
+print(f'{x}-{y}')
+print(f'{x}*{y}')
+print(f'{x}/{y}')
