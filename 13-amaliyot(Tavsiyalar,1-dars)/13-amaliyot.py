2950eaca34015a4c65d2f1a0221d70da7c314c80
O`rinboyev Barkamol <97020905+barkamoljon@users.noreply.github.com>
2022-12-18 04:56:12 +0500

    Create 13-amaliyot.py
    Initial Commit

---
+++ 13-amaliyot(Tavsiyalar,1-dars)/13-amaliyot.py
@@ -0,0 +1,3 @@
+#Github portfolio
+#Bularni githubni o`zida ko`rsatish noqulayroq shuning uchun bunga mustaqil harakat qilib ko`rganing yaxshi.
+#Git va Githubni o`rganish uchun o`zbek tilidagi nazarimdagi eng yaxshi  manbalar:https://youtu.be/GYmLXBlTqfE, https://mohirdev.uz/courses/git-versiya-boshqaruv-tizimi/
